6c976f68f12e0ca7ab28b26564ca3cd5f33a88ba
YueAgar_c <kelvin20040807@gmail.com>
2024-08-30 19:57:39 +0800

    Finish the About section and small adjustments of the Profile section

--- script.js
+++ script.js
@@ -48,15 +48,15 @@ class Navigation {
         this.OpenedMenu = !this.OpenedMenu;
     }
     checkFocus() {
-        if (window.visualViewport.pageTop <= document.getElementById("profile").offsetTop + document.getElementById("profile").offsetHeight / 2) {
+        if (window.visualViewport.pageTop <= document.getElementById("profile").offsetTop + document.getElementById("profile").offsetHeight / 3 * 2) {
             this.Focus = "profile";
-        } else if (window.visualViewport.pageTop <= document.getElementById("about").offsetTop + document.getElementById("about").offsetHeight / 2) {
+        } else if (window.visualViewport.pageTop <= document.getElementById("about").offsetTop + document.getElementById("about").offsetHeight / 3 * 2) {
             this.Focus = "about";
-        } else if (window.visualViewport.pageTop <= document.getElementById("coursework").offsetTop + document.getElementById("coursework").offsetHeight / 2) {
+        } else if (window.visualViewport.pageTop <= document.getElementById("coursework").offsetTop + document.getElementById("coursework").offsetHeight / 3 * 2) {
             this.Focus = "coursework";
-        } else if (window.visualViewport.pageTop <= document.getElementById("projects").offsetTop + document.getElementById("projects").offsetHeight / 2) {
+        } else if (window.visualViewport.pageTop <= document.getElementById("projects").offsetTop + document.getElementById("projects").offsetHeight / 3 * 2) {
             this.Focus = "projects";
-        } else if (window.visualViewport.pageTop <= document.getElementById("contact").offsetTop + document.getElementById("contact").offsetHeight / 2) {
+        } else if (window.visualViewport.pageTop <= document.getElementById("contact").offsetTop + document.getElementById("contact").offsetHeight / 3 * 2) {
             this.Focus = "contact";
         }
     }
@@ -85,7 +85,16 @@ class Language {
             "contact": "Contact",
             "profile-text-1": "Console says: Hello World.",
             "profile-text-2": "I'm YueAgar_c.",
-            "profile-btn-resume": "View my resume"
+            "profile-btn-resume": "View my resume",
+            "about-text-1": "Know more",
+            "about-text-2": "About Me!",
+            "about-text-3": "Education",
+            "about-text-4": "The Chinese University of Hong Kong",
+            "about-text-5": "Bachelor of Science in Computer Science",
+            "about-text-6": "Achievements and Awards",
+            "about-text-7": "Dean's List 2023-24",
+            "about-text-8": "Hello World. I am YueAgar_c (or Yue Ka Leung Kelvin in real life). I am a 20 years old passionate computer science student and I look forward to becoming a professional software engineer in the future.",
+            "working": "Still under construction"
         };
         this.zh = {
             "title": "YueAgar_c的網站",
@@ -95,17 +104,35 @@ class Language {
             "contact": "聯絡",
             "profile-text-1": "控制台：你好世界。",
             "profile-text-2": "我是YueAgar_c。",
-            "profile-btn-resume": "查看我的履歷"
+            "profile-btn-resume": "查看我的履歷",
+            "about-text-1": "了解更多",
+            "about-text-2": "關於我的履歷！",
+            "about-text-3": "教育",
+            "about-text-4": "香港中文大學",
+            "about-text-5": "計算機科學理學士",
+            "about-text-6": "成就與獎項",
+            "about-text-7": "院長嘉許名單 2023-24",
+            "about-text-8": "你好世界。我是YueAgar_c (現實中我的名字是Yue Ka Leung Kelvin)。我是一名20歲的計算機科學學生，正在努力學習並朝着成為專業軟件工程師的目標進發。",
+            "working": "還沒寫好"
         };
         this.jp = {
             "title": "YueAgar_cのウェブサイト",
-            "about": "プロフィール",
+            "about": "私について",
             "coursework": "課題",
             "projects": "プロジェクト",
             "contact": "連絡",
             "profile-text-1": "コンソール：こんにちは、世界。",
             "profile-text-2": "YueAgar_cと申します。",
-            "profile-btn-resume": "履歴書を閲覧する"
+            "profile-btn-resume": "履歴書を閲覧する",
+            "about-text-1": "私についてをもっと",
+            "about-text-2": "知りたいですか？",
+            "about-text-3": "教育",
+            "about-text-4": "香港中文大学",
+            "about-text-5": "情報工学",
+            "about-text-6": "賞",
+            "about-text-7": "成績優秀者 2023-24",
+            "about-text-8": "こんにちは、世界。YueAgar_cです (現実の名前はYue Ka Leung Kelvinです)。私は20歳の情熱的な情報工学学生で、将来はプロのソフトウェアエンジニアになることを楽しみにしています。",
+            "working": "まだ建設中です。"
         };
         this.load();
     }
@@ -125,6 +152,7 @@ class Language {
         document.querySelectorAll(".tr").forEach(element => {
             this[this.language][element.dataset.tr] && (element.innerText = this[this.language][element.dataset.tr]);
         });
+        document.title = this[this.language].title;
         document.querySelector(".menu-links").style.width = this.language == "jp" ? "180px": this.language == "zh" ? "140px" : "auto"; 
     }
 }
